90c1bf5de8d81e94a70baf1d3242998a2158e2de
adri-basterra <abasteru@emeal.nttdata.com>
2023-10-04 15:54:06 +0200

    feat: cache calls

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { QueryClient } from "react-query";
 import {
   BrowserRouter,
   Route,
@@ -9,16 +12,35 @@ import Header from './infrastructure/components/Header/Header';
 import { Home, PodcastDetail } from './infrastructure/views/index';
 
 function App() {
+  const aDayInMiliseconds = 24 * 60 * 60 * 1000;
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: aDayInMiliseconds,
+        cacheTime: aDayInMiliseconds,
+      },
+    },
+  });
+
+  const persister = createSyncStoragePersister({
+    storage: window.localStorage,
+  })
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/podcast/:id" element={<PodcastDetail />} />
-        <Route path="/podcast/:id/episode/:episodeId" element={<PodcastDetail />} />
-      </Routes>
-    </BrowserRouter >
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/podcast/:id" element={<PodcastDetail />} />
+          <Route path="/podcast/:id/episode/:episodeId" element={<PodcastDetail />} />
+        </Routes>
+      </BrowserRouter >
+    </PersistQueryClientProvider >
   );
 }
 
--- src/infrastructure/components/EpisodeDetail/EpisodeDetail.jsx
+++ src/infrastructure/components/EpisodeDetail/EpisodeDetail.jsx
@@ -6,7 +6,7 @@ function EpisodeDetail({ title, url, desc, type }) {
   return (
     <div className="episodeDetail">
       <h2 className="episodeDetail__title">{title}</h2>
-      <p className="episodeDetail__desc">{desc}</p>
+      <p dangerouslySetInnerHTML={{ __html: desc ?? "" }} />
       <audio className="episodeDetail__audio" controls>
         <source src={url} type={type} />
       </audio>
