067f0a85c6fd5817cc1d1f584c07e0e5f65ebf91
adri-basterra <abasteru@emeal.nttdata.com>
2023-10-04 14:50:32 +0200

    feat: podcast detail

--- src/domain/utils/utils.js
+++ src/domain/utils/utils.js
@@ -1,8 +1,30 @@
-
-
 export function searchMatches(search) {
   // Currying to support 'search' assingment just once and use it multiple times
   return (field) => {
     return field.toLowerCase().includes(search.toLowerCase());
   }
 }
+
+// ----------------------------------------------------------------
+
+export function milisecondsToClockMinutes(milliseconds) {
+  if (!milliseconds) return "";
+
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+  hours = hours % 24;
+
+  return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`;
+}
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+// ----------------------------------------------------------------
