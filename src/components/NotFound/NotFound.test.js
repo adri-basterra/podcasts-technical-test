15806713946bf9b7e03f04c51f0f136329c7d79a
adri-basterra <abasteru@emeal.nttdata.com>
2023-10-05 11:14:03 +0200

    feat: NotFound tests

---
+++ src/components/NotFound/NotFound.test.js
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import NotFound from './NotFound';
+
+describe('NotFound', () => {
+  const ELEMENT = "Example";
+
+  test('renders image', () => {
+    render(<NotFound />);
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+  });
+
+  test('renders element', () => {
+    render(<NotFound element={ELEMENT} />);
+    const element = screen.getByRole("heading");
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders correct element', () => {
+    render(<NotFound element={ELEMENT} />);
+    const emptyTitle = screen.getByRole("heading");
+    expect(emptyTitle).toHaveTextContent(`${ELEMENT} not found`);
+  });
+
+})
